22097365a5625d5c0b3a3dbad7a075169dde4f55
s4tar <89261073@qq.com>
2022-05-30 16:25:51 +0800

    update themes

--- custom.el
+++ custom.el
@@ -4,11 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default))
+   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default))
  '(ns-alternate-modifier 'alt)
  '(ns-command-modifier 'meta)
  '(ns-right-alternate-modifier 'super)
- '(package-selected-packages '(use-package sublime-themes doom-themes)))
+ '(package-selected-packages
+   '(github-modern-theme use-package sublime-themes doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
