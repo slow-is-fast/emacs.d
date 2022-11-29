f055c85345f0c1b52467af6064f5d245b3641df3
s4tar <89261073@qq.com>
2022-11-29 14:04:05 +0800

    update

--- custom.el
+++ custom.el
@@ -15,4 +15,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
+ '(default ((t (:inherit nil :stipple nil :background "#ffffff" :foreground "#24292e" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Inconsolata")))))
